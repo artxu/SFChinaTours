5c571672bb2ef2497407d5b78e4fda05a8522f2d
TheUberKid <zekixu@gmail.com>
2015-05-10 12:21:35 -0700

    Base Template
    Uploaded the template.

---
+++ Includes/constants.php
@@ -0,0 +1,14 @@
+<?php
+    // your database's name
+    define("DATABASE", "sfchina");
+
+    // your database's password
+    define("PASSWORD", "avanASEder");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "root");
+
+?>
---
+++ Public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+    render("landing.php", ["title" => "Tour San Francisco"]);
+
+?>
